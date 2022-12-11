84ab881cbb3c516f73e4cbd33eeb04aded709a88
Daniel Torres <dtorres@artemis.cs.csub.edu>
2022-12-11 03:45:13 -0800

    Add transactoin prompt

--- manager_session.php
+++ manager_session.php
@@ -1,8 +1,8 @@
 <?php
-require_once("config.php");
-if(!isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == false){
+    require_once("config.php");
+    if(!isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == false){
     header("Location: signin.php");
-}
+    }
 ?>
 
 <html>
@@ -97,28 +97,80 @@ if(!isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == false){
     ?>
 
     <?php  
-            $db = get_mysqli_connection();
-            $query = $db->prepare("CALL GetMembers(?)");
-            $query->bind_param('s', $_SESSION['SessionID']);
+        $db = get_mysqli_connection();
+        $query = $db->prepare("CALL GetMembers(?)");
+        $query->bind_param('s', $_SESSION['SessionID']);
+        if($query->execute())
+        {
+            $result = $query->get_result();
+            $rows = $result->fetch_all(MYSQLI_ASSOC);
+            echo "You have " . count($rows) . " users in session: " . $_SESSION['SessionID'];
+            echo makeTable($rows);
+            $query->close();
+            echo "<br><br>";
+        }
+        else
+        {
+            echo "Error: " . mysqli_error();
+            echo "Additinal Error: " . mysqli_errno();
+            echo "Even more errors: " . $query->error;
+        }
+    ?>
 
-            if($query->execute())
+    <form method="POST">
+        <label for="category">Transaction Category:</label>
+        <select name="category" id="category">
+            <option value="food">Food</option>
+            <option value="gas">Gas</option>
+            <option value="transportation">Transportation</option>
+            <option value="hotel">Hotel</option>
+            <option value="bars">Bars</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="electronics">Electronics</option>
+            <option value="other">Other</option>
+        </select>
+        <br>
+
+        <label for="item">Item: </label>
+        <input type="text" placeholder="Tacos" name = "item"><br> 
+
+        <label for="price">Price: </label>
+        <input type="text" placeholder = "4.00" name = "price"><br>
+
+        <!--there is a default for today, have if statement to default if nothing is entered -->
+        <label>Date of Transaction: </label>
+        <input type="datetime-local" id="test_datetimelocal" name = "date" ><br>
+
+        <input type="submit" value="Submit" name = "Submit">
+        <br>
+    </form>
+
+    <?php
+        if(isset($_POST["Submit"]) && !empty($_POST["item"]) && !empty($_POST["price"]))
+        {
+            if(!$_POST["date"])
             {
-                $result = $query->get_result();
-                $rows = $result->fetch_all(MYSQLI_ASSOC);
-                echo "You have " . count($rows) . " users in session: " . $_SESSION['SessionID'];
-                echo makeTable($rows);
-                $query->close();
+                date_default_timezone_set('America/Los_Angleles');
+                $_POST["date"] = date('Y-m-d H:i');
+            }
+        
+            $db = get_mysqli_connection();
+            $stmt = $db->prepare("CALL AddTransaction (?,?,?,?,?,?)");
+            $stmt->bind_param('sssssd',$_SESSION['userid'], $_SESSION['SessionID'], $_POST['category'], $_POST["date"],$_POST["item"], $_POST["price"]);
+            if($stmt->execute()){    
+                echo "Transaction added successfully <br>";
+                $stmt->close();
             }
             else
             {
-                echo "Error: " . mysqli_error();
-                echo "Additinal Error: " . mysqli_errno();
-                echo "Even more errors: " . $query->error;
+                echo "Transaction not added<br>";
+                echo "Error: " . $stmt->error . "<br>";
             }
+        }
     ?>
-
+    <br>
     <div class="">
-        <?php
+    <?php
         //DELETE A SESSION THAT USER MANAGES
         //if (!$query->get_result()) {
         //    $delete_form = new PhpFormBuilder();
@@ -146,70 +198,56 @@ if(!isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == false){
         //        }
         //    }
         //}
-        ?>
-        <?php
-            $add_transaction_form = new PhpFormBuilder();
-            $add_transaction_form->set_att("method", "POST");
-            $add_transaction_form->add_input("transaction", array(
-                "type" => "submit",
-                "value" => "Add Transaction"
-            ), "transactionbtn");
-            $add_transaction_form-> add_input("PurchaseType", array(
-                "type" => "text",
-                "placeholder" => "Purchase Type"
-            ), "purchase_type_input");
-            $add_transaction_form->build_form();    
-        ?>
+    ?>
 
-        <h2></h2>
-        <?php  
-            $db = get_mysqli_connection();
-            $query = $db->prepare("CALL GetTransactions(?)");        
-            $query->bind_param('s', $_SESSION['SessionID']);
-
-            if($query->execute())
-            {
-                $result = $query->get_result();
-                $rows = $result->fetch_all(MYSQLI_ASSOC);
-                if(count($rows) > 1 || count($rows) == 0){
-                    echo "There are " . count($rows) . " transactions";
-                }
-                else
-                { 
-                    echo "There is " . count($rows) . " transaction";
-                }
-                
-                echo makeTable($rows);
-                $query->close();
-            }
-            else
-            {
-                echo "Error: " . mysqli_error();
-                echo "Additinal Error: " . mysqli_errno();
-                echo "Even more errors: " . $query->error;
-            }
-    ?>  
-            <h2></h2><h2></h2>
-            <?php
-            $db = get_mysqli_connection();
-            $query = $db->prepare("SELECT SessionID AS 'Joined Sessions\t', Percentage FROM Joins WHERE UserID = ? AND SessionID NOT IN (SELECT SessionID FROM PaypoolSession WHERE UserID = ?)");
-            $query->bind_param('ss', $_SESSION['userid'], $_SESSION['userid'] );
-            $query->execute();
+    <h2></h2>
+    <?php  
+        $db = get_mysqli_connection();
+        $query = $db->prepare("CALL GetTransactions(?)");        
+        $query->bind_param('s', $_SESSION['SessionID']);
+        if($query->execute())
+        {
             $result = $query->get_result();
             $rows = $result->fetch_all(MYSQLI_ASSOC);
-            echo makeTable($rows);
-            
-            if (!$query->get_result()) {
-                echo "Transactions from Joined Sessions:";
-                $query2 = $db->prepare("SELECT TransactionID, concat(FName,' ',LName) AS 'Member', SessionID, ItemPurchased, Price FROM Transaction NATURAL JOIN UserProfile WHERE SessionID IN (SELECT SessionID FROM Joins wherE UserID = ?) AND SessionID NOT IN (SELECT SessionID FROM PaypoolSession WHERE UserID = ?)");  
-                $query2->bind_param('ss', $_SESSION['userid'], $_SESSION['userid']);
-                $query2->execute();
-                $result2 = $query2->get_result();
-                $rows2 = $result2->fetch_all(MYSQLI_ASSOC);
-                echo makeTable($rows2);
+            if(count($rows) > 1 || count($rows) == 0){
+                echo "There are " . count($rows) . " transactions";
             }
+            else
+            { 
+                echo "There is " . count($rows) . " transaction";
+            }
+            
+            echo makeTable($rows);
+            $query->close();
+        }
+        else
+        {
+            echo "Error: " . mysqli_error();
+            echo "Additinal Error: " . mysqli_errno();
+            echo "Even more errors: " . $query->error;
+        }
+    ?>  
+    <h2></h2><h2></h2>
+    <?php
+        $db = get_mysqli_connection();
+        $query = $db->prepare("SELECT SessionID AS 'Joined Sessions\t', Percentage FROM Joins WHERE UserID = ? AND SessionID NOT IN (SELECTSessionID FROM PaypoolSession WHERE UserID = ?)");
+        $query->bind_param('ss', $_SESSION['userid'], $_SESSION['userid'] );
+        $query->execute();
+        $result = $query->get_result();
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        echo makeTable($rows);
+        
+        if (!$query->get_result()) {
+            echo "Transactions from Joined Sessions:";
+            $query2 = $db->prepare("SELECT TransactionID, concat(FName,' ',LName) AS 'Member', SessionID, ItemPurchased, Price FROMTransaction NATURAL JOIN UserProfile WHERE SessionID IN (SELECT SessionID FROM Joins wherE UserID = ?) AND SessionID NOT IN(SELECT SessionID FROM PaypoolSession WHERE UserID = ?)");  
+            $query2->bind_param('ss', $_SESSION['userid'], $_SESSION['userid']);
+            $query2->execute();
+            $result2 = $query2->get_result();
+            $rows2 = $result2->fetch_all(MYSQLI_ASSOC);
+            echo makeTable($rows2);
+        }
                     //php logic to populate with current logged in user sessions
-        ?>
+    ?>
     </div>
 
 </body>
