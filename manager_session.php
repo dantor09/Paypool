e2a37df61ad0930f990f99371bfd5f4bdd3c8217
Daniel Torres <dtorres@artemis.cs.csub.edu>
2023-10-01 22:38:35 -0700

    [Add]: media query for navigation on manager page
    - The navigation on a mobile phone will be collapsed
      into a column view rather than a row view

--- manager_session.php
+++ manager_session.php
@@ -9,7 +9,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Session <?= $_SESSION['SessionID']?> | <?= $PROJECT_NAME?></title>
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/manager_session.css">
